002d21832044bda745604a42f7c4968259416714
stellarhobbes <64601491+stellarhobbes@users.noreply.github.com>
2022-10-04 13:36:17 +0200

    add blogpage

---
+++ src/pages/blog.js
@@ -0,0 +1,11 @@
+import React from "react";
+import GlobalStyle from "../globalStyles";
+
+const BlogPage = () => {
+  return (
+    <body>
+      <GlobalStyle />
+    </body>
+  );
+};
+export default BlogPage;
